a6be0faedb151d68482fe0fb85c0c7ee3ed8f76c
Brad Montgomery <brad@bradmontgomery.net>
2011-01-15 22:22:37 -0600

    Added "tags" and "attributes" fields to the FieldSanitizer class to store the whitelist for bleach, and added some conventient methods to grab those as lists. Also added an html_tags module with some hard-coded lists of commonly used tags. Added an .hgignore too

---
+++ janitor/html_tags.py
@@ -0,0 +1,19 @@
+"""
+This module contains various lists of HTML tags that 
+can be used as a  whitelist for Bleach.
+
+"""
+document_tags = ['body', 'DOCTYPE', 'head', 'html', 'link', 'meta', 'style', 'title',]
+structure_tags = ['div', 'span', ]
+
+basic_content_tags = ['a','abbr', 'acronym', 'blockquote', 'cite', 'code', 'dd', 'del', 'dfn', 'dl', 'dt', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'img', 'ins', 'kbd', 'li', 'ol', 'p', 'pre', 'q', 'samp', 'strong', 'ul', ]
+basic_document_tags = basic_content_tags + document_tags
+
+table_tags = ['caption','col', 'colgroup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr', ]
+form_tags = ['button', 'fieldset', 'form', 'input', 'label', 'legend', 'optgroup', 'option', 'select', 'textarea', ]
+script_tags = ['script', 'noscript', ]
+
+# The Whole Shebang, as listed at http://htmldog.com/reference/htmltags/
+the_whole_shebang = ['a','abbr', 'acronym', 'address', 'area', 'b', 'base', 'bdo', 'big', 'blockquote', 'body', 'br', 'button', 'caption', 'cite', 'code', 'col', 'colgroup', 'dd', 'del', 'dfn', 'div', 'dl', 'DOCTYPE', 'dt', 'em', 'fieldset', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'html', 'hr', 'i', 'img', 'input', 'ins', 'kbd', 'label', 'legend', 'li', 'link', 'map', 'meta', 'noscript', 'object', 'ol', 'optgroup', 'option', 'p', 'param', 'pre', 'q', 'samp', 'script', 'select', 'small', 'span', 'strong', 'style', 'sub', 'sup', 'table', 'tbody', 'td', 'textarea', 'tfoot', 'th', 'thead', 'title', 'tr', 'tt', 'ul', 'var', ]
+
+# Attributes?
--- janitor/models.py
+++ janitor/models.py
@@ -4,18 +4,20 @@ from django.db.models.signals import pre_save
 
 from bleach import Bleach
 
-default_tags = ['html', 'body', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'img', 'a', ]
+import html_tags
 
 def _register(callback, content_type_list):
     for ct in content_type_list:
         pre_save.connect(callback, sender=ct.model_class(), dispatch_uid=ct.model)
 
+def _default_tags():
+    return ','.join(html_tags.basic_content_tags)
+
 class FieldSanitizer(models.Model):
     content_type = models.ForeignKey(ContentType)
     field_name = models.CharField(max_length=255, help_text="The name of a field in the selected Model. It probably should be a TextField or some sublcass of TextField.")
-    # It sure would be nifto to pass this into Bleach.
-    #tags =     
-    #attributes =
+    tags = models.TextField(blank=True, default=_default_tags, help_text="A comma-separated list of HTML tags that are allowed in the selected field")
+    attributes = models.TextField(blank=True, default="alt, class, href, id, src", help_text="A comma-separated list of attributes that are allowed in the selected field")
 
     def __unicode__(self):
         return u"%s - %s" % (self.content_type, self.field_name)
@@ -25,14 +27,23 @@ class FieldSanitizer(models.Model):
         unique_together = (('content_type', 'field_name'), )
 
     def save(self, *args, **kwargs):
+        """
+        Checks to see that ``field_name`` is an attribute of the selected Model,
+        then registers the signal handler with the appropriate model.
+        """
         assert self._field_name_in_model(), "The field_name '%s' does not exist in the model '%s'" % (self.field_name, self.content_type.model)
         super(FieldSanitizer, self).save(*args, **kwargs)
-        # Register the signal with the specified model.
-        _register(sanitize_fields, [self.content_type])
+        _register(sanitize_fields, [self.content_type]) 
 
     def _field_name_in_model(self):
         m = self.content_type.model_class()
         return self.field_name in [f.name for f in m._meta.fields]
+
+    def get_tags_list(self):
+        return [tag.strip() for tag in self.tags.split(',') if len(tag.strip()) > 0]
+
+    def get_attributes_list(self):
+        return [attr.strip() for attr in self.attributes.split(',') if len(attr.strip()) > 0]
 
 def sanitize_fields(sender, **kwargs):
     """
@@ -40,10 +51,6 @@ def sanitize_fields(sender, **kwargs):
 
     ``sender`` - the model class
     ``instance`` - an instance of the sender
-    ``tags`` - a list of tags to allow, e.g. ['b', 'em', 'strong']
-    # Not yet avaiable: ``attributes`` - a list or dict of attributes to allow, e.g. ['href', ] or {'a': ['href']}
-
-    Note: ``tags`` and ``attributes`` are passed directly into Bleach
 
     """
     sender_content_type = ContentType.objects.get_for_model(sender)
@@ -55,13 +62,15 @@ def sanitize_fields(sender, **kwargs):
     for sanitizer in sanitizers:
         if hasattr(sender_instance, sanitizer.field_name):
             field_content = getattr(sender_instance, sanitizer.field_name)
-            if 'tags' in kwargs:
-                field_content = bl.clean(field_content, tags=kwargs['tags'])
+            tags = sanitizer.get_tags_list()
+            attributes = sanitizer.get_attributes_list()
+            if attributes:
+                field_content = bl.clean(field_content, tags=tags, attributes=attributes)
             else:
-                field_content = bl.clean(field_content, tags=default_tags)
+                field_content = bl.clean(field_content, tags=tags)
             setattr(sender_instance, sanitizer.field_name, field_content)
 
-# Register everything
+# Register everything #TODO: can't run syncdb with this here, since the DB tables don't exist yet.
 _content_type_ids = FieldSanitizer.objects.values_list('content_type').distinct()
 _content_types = [ct for ct in ContentType.objects.filter(id__in=_content_type_ids)]
 _register(sanitize_fields, _content_types)
