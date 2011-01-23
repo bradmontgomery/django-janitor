dac94461e8a55702050e3886aeff44bef3de40dc
Brad Montgomery <brad@bradmontgomery.net>
2011-01-22 22:17:54 -0600

    Added setup stuff.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+from janitor import __version__
+
+setup(
+    name='django-janitor',
+    version=__version__,
+    description="django-janitor allows you to use bleach to clean HTML stored in a Model's field.",
+    long_description=open('README.rst').read(),
+    author='Brad Montgomery',
+    author_email='brad@bradmontgomery.net',
+    url='https://bitbucket.org/bkmontgomery/django-janitor/',
+    download_url='https://bitbucket.org/bkmontgomery/django-janitor/get/8292bc72b25f.zip',
+    license='BSD',
+    packages=['janitor'],
+    requires=['django', 'bleach'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Utilities',
+    ]
+)
