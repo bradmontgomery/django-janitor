88fd28e6b391dda5d9d426f1b83fdeaa6fbbe9bf
Brad Montgomery <brad@bradmontgomery.net>
2017-10-30 18:15:57 -0500

    update management commands

--- janitor/management/commands/clean_all.py
+++ janitor/management/commands/clean_all.py
@@ -1,26 +1,16 @@
 """
 Management utility to clean Models that are registered with a FieldSanitizer.
+
 """
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from janitor.models import FieldSanitizer, _clean_class_objects
 
 
-class Command(NoArgsCommand):
-
+class Command(BaseCommand):
     help = "Cleans all Models that have a related FieldSanitizer"
 
-    def __init__(self, *args, **kwargs):
-        super(Command, self).__init__(*args, **kwargs)
-
-        # BaseCommand doesn't have stdout included in Django < 1.2
-        # So, let's add it in if it's not already there.
-        if not hasattr(self, 'stdout'):
-            from sys import stdout
-            self.stdout = stdout
-
-    def handle_noargs(self, **options):
+    def handle(self, *args, **options):
         sanitizers = FieldSanitizer.objects.all()
         klass_list = [fs.content_type.model_class() for fs in sanitizers]
-        object_count = _clean_class_objects(klass_list)
-
-        self.stdout.write("\nCleaned {0} Objects\n".format(object_count))
+        updated = _clean_class_objects(klass_list)
+        self.stdout.write("\nCleaned {0} Objects\n".format(updated))
